e52a6638133116f82d22ba188e2c0178532198d1
Diego <diegoandresdxt@gmai.com>
2026-09-03 10:39:15 -0500

    first commit

--- operaciones.py
+++ operaciones.py
@@ -4,8 +4,12 @@ suma = numero1 + numero2
 resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 division = numero1 / numero2
+resultadosuma = suma + resta + multiplicacion + division
+
 
 print("La suma de los dos números es igual a: ", suma)
 print("La resta de los dos números es igual a: ", resta)
 print("La multiplicacion de los dos números es igual a: ", multiplicacion)
 print("La división de los dos números es igual a: ", division)
+
+print("La suma de los resultados es: ", resultadosuma)
